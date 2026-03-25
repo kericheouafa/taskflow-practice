f675eec4c6b6a9dce7dd0978c7d9b0b6e0e0853c
kericheouafa <ouafakeriche@gmail.com>
2026-03-25 23:45:47 +0100

    J3 -Scanner , boucle while , gestion exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,32 @@
-//java
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         GestionnaireTaches gestionnaire = new GestionnaireTaches();
 
-        gestionnaire.ajouterTache(new Tache("Installer Java", 1));
-        gestionnaire.ajouterTache(new Tache("Apprendre la POO", 2));
-        gestionnaire.ajouterTache(new Tache("Créer une API Spring Boot", 3));
+        System.out.println("=== TASKFLOW ===");
+        System.out.print("Entre le titre de ta tâche : ");
+        String titre = scanner.nextLine();
 
+        int priorite = 0;
+        boolean saisieValide = false;
+
+        while (!saisieValide) {
+            try {
+                System.out.print("Entre la priorité (1, 2 ou 3) : ");
+                priorite = scanner.nextInt();
+                saisieValide = true;
+            } catch (InputMismatchException e) {
+                System.out.println("⚠️ Erreur ! Tape un chiffre : 1, 2 ou 3.");
+                scanner.nextLine();
+            }
+        }
+
+        gestionnaire.ajouterTache(new Tache(titre, priorite));
         gestionnaire.afficherTout();
-        // On termine une tâche
-        gestionnaire.terminerTache("Installer Java");
-
-// On réaffiche
-        System.out.println("=== APRÈS MISE À JOUR ===");
-        gestionnaire.afficherTout();
-
-// On compte ce qui reste
-        System.out.println("Tâches en cours : " + gestionnaire.compterEnCours());
+        scanner.close();
     }
 }
