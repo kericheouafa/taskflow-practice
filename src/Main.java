37426229c157082e7a7b01831f966f935ae20be9
kericheouafa <ouafakeriche@gmail.com>
2026-03-26 12:40:59 +0100

    J3 Menu uneractif , Switch ,Scanner

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,49 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         GestionnaireTaches gestionnaire = new GestionnaireTaches();
+        int choix = 0;
 
-        System.out.println("=== TASKFLOW ===");
-        System.out.print("Entre le titre de ta tâche : ");
-        String titre = scanner.nextLine();
+        while (choix != 4) {
+            System.out.println("\n=== TASKFLOW ===");
+            System.out.println("1. Ajouter une tâche");
+            System.out.println("2. Afficher toutes les tâches");
+            System.out.println("3. Terminer une tâche");
+            System.out.println("4. Quitter");
+            System.out.print("Votre choix : ");
 
-        int priorite = 0;
-        boolean saisieValide = false;
-
-        while (!saisieValide) {
             try {
-                System.out.print("Entre la priorité (1, 2 ou 3) : ");
-                priorite = scanner.nextInt();
-                saisieValide = true;
+                choix = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choix) {
+                    case 1:
+                        System.out.print("Titre : ");
+                        String titre = scanner.nextLine();
+                        System.out.print("Priorité (1, 2 ou 3) : ");
+                        int priorite = scanner.nextInt();
+                        gestionnaire.ajouterTache(new Tache(titre, priorite));
+                        System.out.println("✅ Tâche ajoutée !");
+                        break;
+                    case 2:
+                        gestionnaire.afficherTout();
+                        break;
+                    case 3:
+                        System.out.print("Titre de la tâche à terminer : ");
+                        String t = scanner.nextLine();
+                        gestionnaire.terminerTache(t);
+                        System.out.println("✅ Tâche terminée !");
+                        break;
+                    case 4:
+                        System.out.println("Au revoir ! 👋");
+                        break;
+                    default:
+                        System.out.println("⚠️ Choix invalide !");
+                }
             } catch (InputMismatchException e) {
-                System.out.println("⚠️ Erreur ! Tape un chiffre : 1, 2 ou 3.");
+                System.out.println("⚠️ Tape un chiffre !");
                 scanner.nextLine();
             }
         }
-
-        gestionnaire.ajouterTache(new Tache(titre, priorite));
-        gestionnaire.afficherTout();
         scanner.close();
     }
 }
